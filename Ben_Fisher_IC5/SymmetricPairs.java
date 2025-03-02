f1b77484de49685e513ae2825b98a224ecfc9692
Ben Fisher <bfisher@trinity.edu>
2025-03-02 17:03:03 -0600

    IC6

--- Ben_Fisher_IC5/SymmetricPairs.java
+++ Ben_Fisher_IC5/SymmetricPairs.java
@@ -1,7 +1,27 @@
 package Ben_Fisher_IC5;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 public class SymmetricPairs {
-    public void FindPairs(){
-        
+    public static ArrayList<int[]> FindPairs(int[][] pairs){
+        HashMap<Integer, Integer> pairMap = new HashMap<>();
+        ArrayList<int[]> res = new ArrayList<>();
+        for(int[]pair : pairs){
+            if(pairMap.containsKey(pair[1]) && pairMap.get(pair[1]) == pair[0]){
+                res.add(new int[]{pair[1], pair[0]});
+            }
+            else{
+                pairMap.put(pair[0], pair[1]);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] pairs = {{11, 20}, {30, 40}, {5, 10}, {40, 30}, {10,5}};
+        ArrayList<int[]> symmetricPairs = FindPairs(pairs);
+        System.out.println("Symmetric pairs:");
+        for (int[] pair : symmetricPairs) {
+            System.out.println("(" + pair[0] + ", " + pair[1] + ")");
+        }
     }
 }
