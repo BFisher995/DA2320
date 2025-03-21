29205ec8e1306de9ab767ec3aab6c987970e74a0
Ben Fisher <bfisher@trinity.edu>
2025-03-21 16:58:24 -0500

    IC7

--- Ben_Fisher_IC7/AVLTree.java
+++ Ben_Fisher_IC7/AVLTree.java
@@ -1,5 +1,7 @@
 package Ben_Fisher_IC7;
 
+import java.util.ArrayList;
+
 public class AVLTree {
     public class Node{
 
@@ -8,24 +10,92 @@ public class AVLTree {
         public Node left;
         public Node right;
 
-        public Node(){}
-        public Node(int val){this.val = val;}
+        public Node(){height = 0;}
+        public Node(int val){this.val = val; height = 0;}
         public Node(int val, Node left, Node right){
             this.val = val;
             this.left = left;
             this.right = right;
+            height = 0;
         }
 
-        public int height(){
+        public int getHeight(){
             return height;
         }
 
-        public void updateHeight(){
-
-        }
-
         public int balanceFactor(){
+            return this.right.height - this.left.height;
+        }
+    }
+
+    public static ArrayList<Integer> res = new ArrayList<>();
+    public static void inorderTraversal(Node root){ //copied from IC5
+        if(root != null) {
+            inorderTraversal(root.left);
+            res.add(root.val);
+            inorderTraversal(root.right);
+        }
+    }
+
+    public int calculateHeight(Node n){ //assigns all height values in the tree starting from root n, functions as updateHeight
+        if(n.left == null && n.right == null){
+            n.height = 0;
             return 0;
         }
+        else if (n.right == null){
+            n.height = 1 + calculateHeight(n.left);
+            return 1 + calculateHeight(n.left);
+        }
+        else if (n.left == null){
+            n.height = calculateHeight(n.right);
+            return 1 + calculateHeight(n.right);
+        }
+        n.height = Math.max(1+calculateHeight(n.left), 1+calculateHeight(n.right));
+        return n.height;
+    }
+
+    public void leftRotation(Node m){ //Right-Right rotation
+        Node n = m.right;
+
+        m.right = null;
+        if(n.left != null){
+            m.right = n.left;
+            n.left = m;
+        }
+        else{
+            n.left = m;
+        }
+
+        calculateHeight(n);
+    }
+
+    public void rightRotation(Node m){ //Left-Left rotation
+        Node n = m.left;
+
+        m.right = null;
+        if(n.right != null){
+            m.left = n.right;
+            n.right = m;
+        }
+        else{
+            n.right = m;
+        }
+
+        calculateHeight(n);
+    }
+
+    public void rebalance(Node root, Node n){
+        if(n.balanceFactor() < -1){
+            rightRotation(n);
+        }
+        else if(n.balanceFactor() > 1){
+            leftRotation(n);
+        }
+        res.clear();
+        inorderTraversal(root);
+        for(int item : res){
+            System.out.print(item + ", ");
+        }
+        System.out.println();
     }
 }
